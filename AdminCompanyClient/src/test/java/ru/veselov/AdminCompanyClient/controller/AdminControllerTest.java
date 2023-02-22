0800808bf97006dda6df90b96fb23385862a864f
Veselov Nikolay <veselovnd@gmail.com>
2023-02-22 12:00:39 +0300

    add: try to make test

--- AdminCompanyClient/src/test/java/ru/veselov/AdminCompanyClient/controller/AdminControllerTest.java
+++ AdminCompanyClient/src/test/java/ru/veselov/AdminCompanyClient/controller/AdminControllerTest.java
@@ -1,16 +1,25 @@
 package ru.veselov.AdminCompanyClient.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.reactive.function.client.WebClient;
-@WebMvcTest(controllers = AdminController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class
+
+import java.time.Instant;
+
+@WebMvcTest(controllers = AdminController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class
 })
 class AdminControllerTest {
 
@@ -19,6 +28,20 @@ class AdminControllerTest {
 
     @MockBean
     private WebClient webClient;
+
+    @MockBean
+    private OAuth2AuthorizedClientService auth2AuthorizedClientService;
+
+    @MockBean
+    private OAuth2AuthorizedClient client;
+
+
+    @BeforeEach
+    void init(){
+        ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("test").build();
+        OAuth2AccessToken accessToken= new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER,"test", Instant.now(),Instant.now());
+        client = new OAuth2AuthorizedClient(clientRegistration,null,accessToken);
+    }
 
     @Test
     @Disabled
