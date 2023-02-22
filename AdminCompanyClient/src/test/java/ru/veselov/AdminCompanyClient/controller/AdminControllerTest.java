3704c9ad640927904ca33aa01c4865d611ebfb72
Veselov Nikolay <veselovnd@gmail.com>
2023-02-22 16:45:16 +0300

    test: still finding a way to test controllers

--- AdminCompanyClient/src/test/java/ru/veselov/AdminCompanyClient/controller/AdminControllerTest.java
+++ AdminCompanyClient/src/test/java/ru/veselov/AdminCompanyClient/controller/AdminControllerTest.java
@@ -7,11 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.method.annotation.OAuth2AuthorizedClientArgumentResolver;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -19,8 +28,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Instant;
 
-@WebMvcTest(controllers = AdminController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = AdminController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class
 })
+
+@ActiveProfiles("test")
 class AdminControllerTest {
 
     @Autowired
@@ -31,21 +46,33 @@ class AdminControllerTest {
 
     @MockBean
     private OAuth2AuthorizedClientService auth2AuthorizedClientService;
-
     @MockBean
-    private OAuth2AuthorizedClient client;
+    private OAuth2AuthorizedClientManager auth2AuthorizedClientManager;
+    @MockBean
+    private ClientRegistrationRepository clientRegistrationRepository;
+    @MockBean
+    private SecurityFilterChain securityFilterChain;
 
-
-    @BeforeEach
-    void init(){
-        ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("test").build();
-        OAuth2AccessToken accessToken= new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER,"test", Instant.now(),Instant.now());
-        client = new OAuth2AuthorizedClient(clientRegistration,null,accessToken);
-    }
+    //todo сделать внедрение юзера в контекст
 
     @Test
     @Disabled
     public void adminPageTest() throws Exception {
+
+        ClientRegistration test = ClientRegistration.withRegistrationId("test").clientId("test")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .clientSecret("test")
+                .authorizationUri("test")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .redirectUri("test")
+                .tokenUri("test")
+
+                .build();
+        OAuth2AuthorizedClient client = new OAuth2AuthorizedClient(
+                test,"name", mock(OAuth2AccessToken.class)
+        );
+        when(auth2AuthorizedClientService.loadAuthorizedClient(anyString(),anyString())).thenReturn(client);
+
         mockMvc.perform(MockMvcRequestBuilders.get("/admin"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("adminPage"));
